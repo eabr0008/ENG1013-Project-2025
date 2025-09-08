8f48a535f9d7cca5ab055cc0dffab50dd716f118
eabr0008 <eabr0008@student.monash.edu>
2025-09-08 19:45:47 +1000

    1.3

--- sub2_m2.py
+++ sub2_m2.py
@@ -1,7 +1,7 @@
 # Initial prototype of subsystem 2 milestone 2 code
 # Created By : Eden Abrahams & Majd Abou Zaki
-# Created Date: 05/09/2025
-# version = 1.2
+# Last Edited Date: 08/09/2025
+# version = 1.3
 from pymata4 import pymata4
 board = pymata4.Pymata4()
 import time
@@ -74,3 +74,75 @@ make(tl4G, 1); make(tl4Y, 0); make(tl4R, 0)
 make(tl5G, 0); make(tl5Y, 0); make(tl5R, 1)
 make(pl1G, 0); make(pl1R, 1)
 make(pl2G, 0); make(pl2R, 1)
+
+lightState = "TL4_Green"
+startTime = time.time()
+
+pedPressed = False #is a pedestrian button pressed
+pedPressTime = None #time at press
+pedPrinted = False #has this been printed 
+currentPreemptedRoad = None   # "TL4" or "TL5" during a preempt yellow
+
+
+try:
+    while True:
+        #at any point in time, the current time is defined
+        #time so far elapsed is then established as current - start
+        currentTime = time.time()
+        elapsedTime = currentTime - startTime
+        #reading the button presses and printing accordingly
+        if board.digital_read(pb1)[0] == 1 or board.digital_read(pb2)[0] == 1:
+            if not pedPressed:
+                pedPressed = True 
+                pedPressTime = currentTime
+                #avoid multiple presses affecting prints and cycles via tracking of printed
+                if not pedPrinted:
+                    if board.digital_read(pb1)[0] == 1:
+                        print("PB1 was pressed")
+                        pedPrinted = True
+                    else:
+                        print("PB2 was pressed")
+                        pedPrinted = True
+        if pedPressed and (currentTime - pedPressTime >= pedDelayBeforeSequence):
+        #checked if button is pressed that elapsed time of 2 seconds has passed
+            if lightState not in ("TL5_Red"): #2.R1 if TL5 is not red, goes yellow, getting ready for peds
+                currentPreemptedRoad = "TL5"
+                make(tl5G, 0); make(tl5Y, 1); make(tl5R, 0)
+                make(tl4G, 0); make(tl4Y, 0); make(tl4R, 1)
+            else:
+                currentPreemptedRoad = "TL4" #then now make TL4 yellow, getting ready for peds
+                make(tl4G, 0); make(tl4Y, 1); make(tl4R, 0)
+                make(tl5G, 0); make(tl5Y, 0); make(tl5R, 1)
+            state = "Preempt_Yellow"
+            startTime = currentTime
+            pedPrinted = False
+        
+        #setting up a loop for when nothing is pressed
+        if lightState == "TL4_Green":
+            if elapsedTime >= durTl4Green: #if tl4 has been green for 20s
+                make(tl4G, 0); make(tl4Y, 1); make(tl4R, 0)
+                state = "TL4_Yellow"
+                startTime = currentTime
+        elif state == "TL4_Yellow":
+            if elapsedTime >= durTl4Yellow:
+                make(tl4G, 0); make(tl4Y, 0); make(tl4R, 1)
+                make(tl5G, 1); make(tl5Y, 0); make(tl5R, 0)
+                state = "TL5_Green"
+                startTime = currentTime
+        elif state == "TL5_Green":
+            if elapsedTime >= durTl4Yellow #check if 3s have passed, make tl5 yellow
+                make(tl5G, 0); make(tl5Y, 1); make(tl5R, 0)
+                state = "TL5_Yellow"
+                startTime = currentTime
+        elif state == "TL5_Yellow":
+            if elapsedTime >= durTl5Yellow:
+                make(tl4G, 1); make(tl4Y, 0); make(tl4R, 0)
+                make(tl5G, 0); make(tl5Y, 0); make(tl5R, 1)
+                state = "TL4_Green"
+                startTime = currentTime
+        #pedestrian actions now:
+        elif state ==
+except KeyboardInterrupt:
+    turn_off()
+    board.shutdown()
+
