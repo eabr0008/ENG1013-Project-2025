0e51236cc9d1e81365e1b9d9bacc158d849859ef
eabr0008 <eabr0008@student.monash.edu>
2025-09-09 11:49:45 +1000

    1.6

--- sub2_m2.py
+++ sub2_m2.py
@@ -1,7 +1,12 @@
 # Initial prototype of subsystem 2 milestone 2 code
 # Created By : Eden Abrahams & Majd Abou Zaki
 # Last Edited Date: 08/09/2025
-# version = 1.4
+# version = 1.6
+'''
+Used chatGPT 5 in the lines 174,176,177, where it was quite difficult 
+to find an elegent way to make the pedestrian light flash smoothly
+https://chatgpt.com/s/t_68bf7de1f4f08191a590d628d14a0a68
+'''
 from pymata4 import pymata4
 board = pymata4.Pymata4()
 import time
@@ -42,12 +47,12 @@ durTl5Yellow = 3 #tl5 is yellow for 3 seconds
 #pedestrian lights:
 pedDelayBeforeSequence = 2 #wait 2 seconds before pedestrian actions
 durPedGreen = 3 #pedestrian lights should turn green for 3 seconds
-durPedFlash = 2 #flashes red for 2 seconds
-flashFreq = 3 #just chose a random frequency for flashing red
+durPedFlash = 2 #ped flashes red for 2 seconds 
+flashHz = 3 #flash 3 times per second
 
 #initialisations for inputs
-board.set_pin_mode_digital(pb1)
-board.set_pin_mode_digital(pb2)
+board.set_pin_mode_digital_input_pullup(pb1)
+board.set_pin_mode_digital_input_pullup(pb2)
 
 #set the pins to digital output (for LEDs)
 for pin in ledPinNums:
@@ -77,7 +82,7 @@ make(tl5G, 0); make(tl5Y, 0); make(tl5R, 1)
 make(pl1G, 0); make(pl1R, 1)
 make(pl2G, 0); make(pl2R, 1)
 
-lightState = "TL4_Green"
+state = "TL4_Green"
 startTime = time.time()
 
 pedPressed = False #is a pedestrian button pressed
@@ -92,22 +97,25 @@ try:
         #time so far elapsed is then established as current - start
         currentTime = time.time()
         elapsedTime = currentTime - startTime
+        # read buttons (active-low, 0 means is pressed)
+        pb1Pressed = (board.digital_read(pb1)[0] == 0)
+        pb2Pressed = (board.digital_read(pb2)[0] == 0)
         #reading the button presses and printing accordingly
-        if board.digital_read(pb1)[0] == 1 or board.digital_read(pb2)[0] == 1:
-            if not pedPressed:
-                pedPressed = True 
-                pedPressTime = currentTime
-                #avoid multiple presses affecting prints and cycles via tracking of printed
-                if not pedPrinted:
-                    if board.digital_read(pb1)[0] == 1:
-                        print("PB1 was pressed")
-                        pedPrinted = True
-                    else:
-                        print("PB2 was pressed")
-                        pedPrinted = True
-        if pedPressed and (currentTime - pedPressTime >= pedDelayBeforeSequence):
-        #checked if button is pressed that elapsed time of 2 seconds has passed
-            if lightState not in ("TL5_Red"): #2.R1 if TL5 is not red, goes yellow, getting ready for peds
+        if (pb1Pressed or pb2Pressed) and not pedPressed :
+            pedPressed = True 
+            pedPressTime = currentTime
+            #avoid multiple presses affecting prints and cycles via tracking of printed
+            if not pedPrinted:
+                if pb1Pressed:
+                    print("PB1 was pressed")
+                    pedPrinted = True
+                else:
+                    print("PB2 was pressed")
+                    pedPrinted = True
+        if pedPressed and (currentTime - pedPressTime >= pedDelayBeforeSequence) and state not in ("Current_Stream_Yellow", "Ped_Green", "Ped_Flash_Red"):
+            #final checks before initialising pedestrian actions
+            #checked if button is pressed that elapsed time of 2 seconds has passed
+            if state in ("TL5_Green", "TL5_Yellow"): #2.R1 if TL5 is not red, goes yellow, getting ready for peds
                 currentWayEnding = "TL5"
                 make(tl5G, 0); make(tl5Y, 1); make(tl5R, 0)
                 make(tl4G, 0); make(tl4Y, 0); make(tl4R, 1)
@@ -120,7 +128,7 @@ try:
             pedPrinted = False
         
         #setting up a loop for when nothing is pressed
-        if lightState == "TL4_Green":
+        if state == "TL4_Green":
             if elapsedTime >= durTl4Green: #if tl4 has been green for 20s
                 make(tl4G, 0); make(tl4Y, 1); make(tl4R, 0)
                 state = "TL4_Yellow"
@@ -132,7 +140,7 @@ try:
                 state = "TL5_Green"
                 startTime = currentTime
         elif state == "TL5_Green":
-            if elapsedTime >= durTl4Yellow: #check if 3s have passed, make tl5 yellow
+            if elapsedTime >= durTl5Green: #check if 3s have passed, make tl5 yellow
                 make(tl5G, 0); make(tl5Y, 1); make(tl5R, 0)
                 state = "TL5_Yellow"
                 startTime = currentTime
@@ -146,30 +154,42 @@ try:
         #now that the current stream of traffic has turned yellow for 3s,
         #all traffic can turn red and peds can cross
         elif state == "Current_Stream_Yellow":
-            if currentTime >= durTl5Yellow and currentWayEnding == "TL5": 
+            if elapsedTime >= durTl5Yellow and currentWayEnding == "TL5": 
                 make(tl5G, 0); make(tl5Y, 0); make(tl5R, 1)
                 make(pl1G, 1); make(pl1R, 0)
                 make(pl2G, 1); make(pl2R, 0)
                 state = "Ped_Green"
                 startTime = currentTime
-            elif currentTime >= durTl4Yellow and currentWayEnding == "TL4": 
+            elif elapsedTime >= durTl4Yellow and currentWayEnding == "TL4": 
                 make(tl4G, 0); make(tl4Y, 0); make(tl4R, 1)
                 make(pl1G, 1); make(pl1R, 0)
                 make(pl2G, 1); make(pl2R, 0)
                 state = "Ped_Green"
                 startTime = currentTime
         elif state == "Ped_Green":
-            if currentTime >= durPedGreen:
+            if elapsedTime >= durPedGreen:
                 make(pl1G, 0); make(pl2G, 0)
                 state = "Ped_Flash_Red"
                 startTime = currentTime
         elif state == "Ped_Flash_Red":
-            #need to add a way to make it flash
-            pedPressed = False; pedPressTime = None
+            #to work out if LED should be one or off
+            #multiplying by flashHz turns it into flashes per second
+            #int() makes sure that is is only 1 or 0
+            phase = int((elapsedTime) * flashHz) % 2
+            #if phase is 1, then it will turn on, otherwise it will be off
+            make(pl1R, 1 if phase else 0)
+            make(pl2R, 1 if phase else 0)
+            if elapsedTime >= durPedFlash:
+                make(pl1R, 1); make(pl2R, 1)
+                make(tl4G, 1); make(tl4Y, 0); make(tl4R, 0)
+                make(tl5G, 0); make(tl5Y, 0); make(tl5R, 1)
+                state = "TL4_Green"
+                startTime = currentTime
+                currentWayEnding = None
+                pedPressed = False
+                pedPressTime = None
             time.sleep(0.1)
-
-            
-            
+       
 except KeyboardInterrupt:
     turn_off()
     board.shutdown()
